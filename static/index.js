4ffaa0d76549a2029abac9ec6b5f8a61f10e7fa9
Aaron <Aaron@Aarons-MacBook-Air.local>
2022-09-06 22:52:49 +0100

    steps towards saving input data to json file

--- static/index.js
+++ static/index.js
@@ -1,7 +1,23 @@
-/* import translate.py here */
+import * as fsPromises from 'node:fs/promises';
 
 console.log("I am running.");
 
-function translate() {
+async function translate() {
+    var inputData = {
+        srcLangText : null,
+        srcLang : null,
+        destLang : null
+    };
+    inputData['srcLangText'] = document.getElementById('src_lang_input').value;
+    inputData['srcLang'] = document.getElementById('input-lang-box').value;
+    inputData['destLang'] = document.getElementById('output-lang-box').value;
 
+    var jsonInputData = JSON.stringify(inputData);
+    /* save json input data to json file, which is to be stored in ../data directory (relative path to this file) */
+    var fileHandle = fsPromises.open('../data/input_data.json');
+    const promise = fsPromises.writeFile(fileHandle, jsonInputData);
+    await promise;
+
+    /* get translated output from other json file (to be stored in save directory by translate.py), parse it into a 
+    js object and write the destination language's text to the output box on the webpage */
 }
--- translate.py
+++ translate.py
@@ -4,4 +4,6 @@ translator = Translator()
 
 # Here, I need to pass the input text from the user input, in the html, to the translate() function AND ensure the 
 # src and dest language parameters of the translate() function match that of the drop-down boxes in the html document
-# translator.translate(input text, dest lang as string (language code from googletrans required), src lang as string)
+# The returned value, translated, will need to converted back to json and saved into a different json file to the input json file from 
+# the js file. 
+# translated = translator.translate(input text, dest lang as string (language code from googletrans required), src lang as string)
