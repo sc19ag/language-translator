620d3618ecbe08bea21e2f523b63c8d3451c5bde
Aaron <Aaron@Aarons-MacBook-Air.local>
2022-09-07 15:06:38 +0100

    functionality to flask app added

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
-from translate import *
+from urllib import request
+from flask import Flask, render_template, jsonify
+from googletrans import Translator
 
 app = Flask(
     __name__,
@@ -10,6 +11,31 @@ app = Flask(
 @app.route("/")
 def render_html():
     return render_template('index.html')
+
+@app.route("/translate", methods=['POST'])
+def translate_func():
+    data = request.get_json()
+
+    # converts full language string of src and dest langs from client into one of googletrans's language codes
+    if data['srcLang'] == 'English':
+        data['srcLang'] = 'en'
+    elif data['srcLang'] == 'French':
+        data['srcLang'] = 'fr'
+    elif data['srcLang'] == 'Turkish':
+        data['srcLang'] = 'tr'
+    
+    if data['destLang'] == 'English':
+        data['destLang'] = 'en'
+    elif data['destLang'] == 'French':
+        data['destLang'] = 'fr'
+    elif data['destLang'] == 'Turkish':
+        data['destLang'] = 'tr'
+
+    translator = Translator()
+    translated = translator.translate(data['srcLangText'], data['srcLang'], data['destLang'])
+
+    return jsonify(translated)
+
 
 if __name__ == '__main__':
     app.run()
--- static/index.js
+++ static/index.js
@@ -1,22 +1,32 @@
-import * as fsPromises from 'node:fs/promises';
+/* import * as fsPromises from 'node:fs/promises'; */
 
 console.log("I am running.");
 
-async function translate() {
+function translate() {
     var inputData = {
-        srcLangText : null,
-        srcLang : null,
-        destLang : null
+        srcLangText : document.getElementById('src_lang_input').value,
+        srcLang : document.getElementById('input-lang-box').value,
+        destLang : document.getElementById('output-lang-box').value
     };
+    /*    
     inputData['srcLangText'] = document.getElementById('src_lang_input').value;
     inputData['srcLang'] = document.getElementById('input-lang-box').value;
-    inputData['destLang'] = document.getElementById('output-lang-box').value;
+    inputData['destLang'] = document.getElementById('output-lang-box').value; */
 
     var jsonInputData = JSON.stringify(inputData);
+    
+    /* send jsonInputData to /translate resource in flask app via a http POST request */
+
+
+    /* receive the response from the http POST request containing the translated text in json form and display this in the 
+    output box on the webpage */
+
+
+    
     /* save json input data to json file, which is to be stored in ../data directory (relative path to this file) */
-    var fileHandle = fsPromises.open('../data/input_data.json');
-    const promise = fsPromises.writeFile(fileHandle, jsonInputData);
-    await promise;
+    /* var fileHandle = fsPromises.open('../data/input_data.json');
+    const promise = fsPromises.writeFile('../data/input_data.json', jsonInputData); 
+    await promise; */
 
     /* get translated output from other json file (to be stored in save directory by translate.py), parse it into a 
     js object and write the destination language's text to the output box on the webpage */
