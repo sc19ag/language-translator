d115515ff110c7116044a6b85dbb909c895c21dc
Aaron <Aaron@Aarons-MacBook-Air.local>
2022-09-13 19:37:05 +0100

    simplified app.py and added more languages

--- app.py
+++ app.py
@@ -21,21 +21,6 @@ def translate_func():
         error = {'errorMessage' : 'Error at /translate: request header is not application/json'}
         return fl.jsonify(error)
 
-    # converts full language string of src and dest langs, from client, into one of googletrans's language codes
-    if data['srcLang'] == 'English':
-        data['srcLang'] = 'en'
-    elif data['srcLang'] == 'French':
-        data['srcLang'] = 'fr'
-    elif data['srcLang'] == 'Turkish':
-        data['srcLang'] = 'tr'
-    
-    if data['destLang'] == 'English':
-        data['destLang'] = 'en'
-    elif data['destLang'] == 'French':
-        data['destLang'] = 'fr'
-    elif data['destLang'] == 'Turkish':
-        data['destLang'] = 'tr'
-
     translator = Translator()
     translated = translator.translate(data['srcLangText'], data['destLang'], data['srcLang'])
 
